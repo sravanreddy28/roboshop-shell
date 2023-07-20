2db982ffb10aa5fc80f0206f12109eb511f884f4
sravan k <sravankakireddy@gmail.com>
2023-07-20 12:11:52 +0530

    Roboshop scripts Update DNS Records

--- shipping.sh
+++ shipping.sh
@@ -11,6 +11,7 @@ mvn clean package
 mv target/shipping-1.0.jar shipping.jar
 
 yum install mysql -y
+mysql -h mysql.sdevops28.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
 
 systemctl enable shipping
 systemctl restart shipping
