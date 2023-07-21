456ea433c4e482ab7dfb4f3bec695b06e20cae3f
sravan k <sravankakireddy@gmail.com>
2023-07-21 15:36:40 +0530

    user.sh taril 3

--- user.sh
+++ user.sh
@@ -1,42 +1,42 @@
-echo ">>>>> user service file <<<<< "
+echo -e "\e[36m>>>>> user service file <<<<<\e[0m "
 cp user.service /etc/systemd/system/user.service
 
-echo ">>>>> copying mongo repos <<<<<"
+echo -e "\e[32m>>>>> copying mongo repos <<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
-echo ">>>>> download nodejs rpm <<<<<"
+echo -e "\e[33m>>>>> download nodejs rpm <<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
-echo ">>>>> install nodejs <<<<<"
+echo -e "\e[34m>>>>> install nodejs <<<<<\e[0m"
 yum install nodejs -y
 
-echo ">>>>> adding user <<<<<"
+echo -e "\e[36m>>>>> adding user <<<<<\e[0m"
 useradd roboshop
 
-echo ">>>>> remove directioy <<<<<"
+echo -e "\e[36m>>>>> remove directioy <<<<<\e[0m"
 rm -rf /app
 
-echo ">>>>> create directioy <<<<<"
+echo -e "\e[35m>>>>> create directioy <<<<<\e[0m"
 mkdir /app
 
-echo ">>>>> download artificats <<<<<"
+echo -e "\e[33m>>>>> download artificats <<<<<\e[0m"
 curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 
-echo ">>>>> extract artifacts <<<<<"
+echo -e "\e[32m>>>>> extract artifacts <<<<<\e[0m"
 cd /app
 unzip /tmp/user.zip
 cd /app
 
-echo ">>>>> install nodejs dependencies <<<<<"
+echo -e "\e[36m>>>>> install nodejs dependencies <<<<<\e[0m"
 npm install
 
-echo ">>>>> install mongodb org shell <<<<<"
+echo -e "\e[35m>>>>> install mongodb org shell <<<<<\e[0m"
 yum install mongodb-org-shell -y
 
-echo ">>>>> load user schema <<<<<"
+echo -e "\e[35m>>>>> load user schema <<<<<\e[0m"
 mongo --host mongodb.sdevops28.online </app/schema/user.js
 
-echo ">>>>> enable restart user <<<<<"
+echo -e "\e[33m>>>>> enable restart user <<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
