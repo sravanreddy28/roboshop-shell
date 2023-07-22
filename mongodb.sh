bae365b98928c8e5d9da9da9724c699424f7a9e1
sravan k <sravankakireddy@gmail.com>
2023-07-22 12:33:07 +0530

    update mongobd.sh 1

--- mongodb.sh
+++ mongodb.sh
@@ -1,5 +1,11 @@
+echo ">>>>>>>>>> mongbd repo <<<<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo ">>>>>>>>>> install mongodb <<<<<<<<<<"
 yum install mongodb-org -y
+
 # update listen address from 127.0.0.1 to 0.0.0.0
+
+echo ">>>>>>>>>> enable start mongob <<<<<<<<<<"
 systemctl enable mongod
 systemctl restart mongod
