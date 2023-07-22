326207c0e72e880aeaab1988571e3089abf42dd2
sravan k <sravankakireddy@gmail.com>
2023-07-22 10:37:10 +0530

    update fronted.sh 2

--- catalogue.sh
+++ catalogue.sh
@@ -1,45 +1,43 @@
-log=/tmp/roboshop.log
-
 echo -e "\e[36m>>>>>>>>>>> create catalogue service <<<<<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service &>>${log}
+cp catalogue.service /etc/systemd/system/catalogue.service
 
 echo -e "\e[36m>>>>>>>>>>>> create mongodb repo <<<<<<<<<<<\e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[36>>>>>>>>>>> install  nodejs repo <<<<<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 echo -e "\e[36>>>>>>>>>> install nodejs <<<<<<<<<<\e[0m"
-yum install nodejs -y &>>${log}
+yum install nodejs -y
 
 echo -e "\e[36>>>>>>>>>> create application user <<<<<<<<<<\e[0m"
-useradd roboshop &>>${log}
+useradd roboshop
 
 echo -e "\e[36>>>>>>>>>>> remove directory <<<<<<<<<<\e[0m"
-rm -rf /app &>>${log}
+rm -rf /app
 
 echo -e "\e[36>>>>>>>>>>> create directory <<<<<<<<<<\e[0m"
-mkdir /app &>>${log}
+mkdir /app
 
 echo -e "\e[36>>>>>>>>>> download application content <<<<<<<<<<\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 
 echo -e "\e[36>>>>>>>>>> extract applicaation content <<<<<<<<<<\e[0m "
 cd /app
-unzip /tmp/catalogue.zip &>>${log}
+unzip /tmp/catalogue.zip
 cd /app
 
 echo -e "\e[36>>>>>>>>>> download nodejs dependency <<<<<<<<<<\e[0m"
-npm install &>>${log}
+npm install
 
 echo -e "\e[36 >>>>>>>>>> install mongodb org shell <<<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y &>>${log}
+yum install mongodb-org-shell -y
 
 echo -e "\e[36>>>>>>>>>> load catalogue schema <<<<<<<<<<\e[0m"
-mongo --host mongodb.sdevops28.online </app/schema/catalogue.js &>>${log}
+mongo --host mongodb.sdevops28.online </app/schema/catalogue.js
 
 echo -e "\e[36>>>>>>>>>> enable start catalouge <<<<<<<<<<\e[0m"
-systemctl daemon-reload &>>${log}
-systemctl enable catalogue &>>${log}
-systemctl restart catalogue &>>${log}
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
 
