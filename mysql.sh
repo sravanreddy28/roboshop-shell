2898ea01c9e26aebaf52a022c8c84886a49b1b95
sravan k <sravankakireddy@gmail.com>
2023-07-22 12:36:44 +0530

    update mysql.sh 1

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,15 @@
+echo ">>>>>>>>>> mysql.repos <<<<<<<<<<"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
+
+echo ">>>>>>>>>> disable msql <<<<<<<<<<"
 yum module disable mysql -y
+
+echo ">>>>>>>>>> install mqsql community server <<<<<<<<<<"
 yum install mysql-community-server -y
+
+echo ">>>>>>>>>> enable start mysql <<<<<<<<<<"
 systemctl enable mysqld
 systemctl start mysqld
+
+echo ">>>>>>>>>> setting password <<<<<<<<<<"
 mysql_secure_installation --set-root-pass RoboShop@1
