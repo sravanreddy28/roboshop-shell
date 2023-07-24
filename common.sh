f3bc14bb9e993705c5149fceb79014492da02997
sravan k <sravankakireddy@gmail.com>
2023-07-24 09:28:48 +0530

    functions common.sh 2

--- common.sh
+++ common.sh
@@ -1,4 +1,4 @@
-nodejs() {
+func_nodejs() {
 log=\tmp\roboshop.log
 
 echo -e "\e[36m>>>>> user service file <<<<<\e[0m "
@@ -46,3 +46,38 @@ systemctl restart ${component} &>>${log}
 
 }
 
+func_java() {
+
+echo -e "\e[25m>>>>> user service file <<<<<\e[0m "
+cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
+
+echo -e "\e[33m>>>>> install maven<<<<<\e[0m "
+yum install maven -y &>>${log}
+
+echo -e "\e[36m>>>>> add application user <<<<<\e[0m "
+useradd roboshop &>>${log}
+
+echo -e "\e[26m>>>>> creating directory <<<<<\e[0m "
+mkdir /app &>>${log}
+echo -e "\e[40m>>>>> download artifacts <<<<<\e[0m "
+curl -L -o /tmp/${component}zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
+
+echo -e "\e[38m>>>>> extract artifacts <<<<<\e[0m "
+cd /app
+unzip /tmp/${component}.zip &>>${log}
+cd /app
+
+echo -e "\e[35m>>>>> jar file <<<<<\e[0m "
+mvn clean package &>>${log}
+mv target/${component}-1.0.jar ${component}.jar &>>${log}
+
+echo -e "\e[34m>>>>> install mysql <<<<<\e[0m "
+yum install mysql -y &>>${log}
+
+ echo -e "\e[33m>>>>> load schema <<<<<\e[0m "
+mysql -h mysql.sdevops28.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
+
+echo -e "\e[32m>>>>> enable start service <<<<<\e[0m "
+systemctl enable ${component} &>>${log}
+systemctl restart ${component} &>>${log}
+}
--- shipping.sh
+++ shipping.sh
@@ -1,17 +1,4 @@
-cp shipping.service /etc/systemd/system/shipping.service
+component=shipping
+source common.sh
 
-yum install maven -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-cd /app
-unzip /tmp/shipping.zip
-cd /app
-mvn clean package
-mv target/shipping-1.0.jar shipping.jar
-
-yum install mysql -y
-mysql -h mysql.sdevops28.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
-
-systemctl enable shipping
-systemctl restart shipping
+func_java
