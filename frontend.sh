febee26e0b78086c5e48a9e21c6c822c58200762
sravan k <sravankakireddy@gmail.com>
2023-07-25 10:50:43 +0530

    var 1

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,13 @@
+echo -e "\e[35m>>>>> install mongodb org shell <<<<<\e[0m"
 yum install nginx -y
+
+echo -e "\e[35m>>>>> install mongodb org shell <<<<<\e[0m"
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "\e[35m>>>>> install mongodb org shell <<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[35m>>>>> install mongodb org shell <<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
